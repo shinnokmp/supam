424451442bb83e4231fc66fc111c2ea929442e06
Itang Samuel <31269938+shinnokmp@users.noreply.github.com>
2018-06-26 14:47:47 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,8 +1,6 @@
-// Select color input
-// Select size input
-
 // When size is submitted by the user, call makeGrid()
 
+//Javascript code used to select color input
 function changeColor() {
     const color = document.getElementById("colorPicker").value;
     this.style.background = color;
@@ -15,7 +13,8 @@ function makeGrid() {
     const gridWidth = document.getElementById("input_width").value;
     const pixelCanvas = document.getElementById("pixel_canvas"); 
     pixelCanvas.innerText=""; // this is an empty table
-    
+
+//Javascript code to select and create size input of table and each cell contains onclick event handler that calls the changeColor() function.
     for (let c=0; c<gridHeight; ++c) {
         const row = pixelCanvas.insertRow(-1); // insert new row at the last position
         for (let r=0; r<gridWidth; ++r) {
@@ -23,8 +22,10 @@ function makeGrid() {
             column.onclick = changeColor;
         }
     }
-    event.preventDefault();
+    event.preventDefault(); //Javascript code that ensures the behaviour stays without going to default after the event.
 }
+
+//Javascript code to alert the coder and contributors to this code
 function credits() {
     alert("Please take out time to appreciate the coder, ITANG, SAMUEL ITANG with the help of mentors and other scholars at Udacity");
 }
